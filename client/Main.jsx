9b639e6bb60988c1a04dc013ba67d06eed026eba
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-04-20 21:58:03 +0530

    client initial commit

---
+++ client/Main.jsx
@@ -0,0 +1,30 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+} from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
+const Main = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Group>
+          <Stack.Screen name="home" component={<Home />}></Stack.Screen>
+          <Stack.Screen name="login" component={<Home />}></Stack.Screen>
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Main;
+
+const styles = StyleSheet.create({});
